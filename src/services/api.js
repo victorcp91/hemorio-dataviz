f7628e138a3946f9505e4fb610de254a4fc25375
Victor Corrêa Pereira <victor@MacBook-Air-de-Victor.local>
2024-11-26 12:56:33 -0300

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,56 +1,63 @@
-import axios from 'axios';
+import axios from "axios";
 
 class Api {
-  constructor(){
+  constructor() {
     this.api = axios.create({
-      baseURL: 'https://hemorio-api.herokuapp.com'
+      baseURL: "https://hemorio-dataviz.vercel.app/",
     });
   }
 
   getCountries = () => {
     return axios
-      .get('https://restcountries.eu/rest/v2/all').then(res => {
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((res) => {
         return res.data;
-      }).catch(err => {
+      })
+      .catch((err) => {
         console.error(err);
       });
-  }
+  };
 
-  getBloodBanks(){
-    return this.api.get('/blood_bank/?approved=true').then(res => {
-      return res.data;
-    }).catch(err => {
-      console.error(err);
-      return err;
-    });
+  getBloodBanks() {
+    return this.api
+      .get("/blood_bank/?approved=true")
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.error(err);
+        return err;
+      });
   }
 
   sendBloodBankFile = (file) => {
     const bloodBankFile = new FormData();
-    bloodBankFile.append('doc', file);
+    bloodBankFile.append("doc", file);
     return axios({
       method: "post",
-      url: "https://hemorio-api.herokuapp.com/upload",
+      url: "https://hemorio-dataviz.vercel.app/upload",
       data: bloodBankFile,
       headers: { "Content-Type": "multipart/form-data" },
     })
-    .then(function (res) {
-      return res.data;
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-  }
+      .then(function (res) {
+        return res.data;
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
   createBloodBank = (info) => {
-    return this.api.post('/blood_bank', info).then(res => {
-      return res.data;
-    }).catch(err => {
-      console.error(err);
-      return err;
-    });
-  }
-
+    return this.api
+      .post("/blood_bank", info)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.error(err);
+        return err;
+      });
+  };
 }
 
 export default new Api();
